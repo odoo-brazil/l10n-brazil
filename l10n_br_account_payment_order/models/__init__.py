d04f62853069fc24adfb5f90041b8f3f6da65851
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-09-18 13:21:53 -0300

    [ADD] Account Payment Mode Line

--- l10n_br_account_payment_order/models/__init__.py
+++ l10n_br_account_payment_order/models/__init__.py
@@ -7,6 +7,7 @@ from . import account_invoice
 from . import account_move
 from . import account_move_line
 from . import account_payment_mode
+from . import account_payment_mode_line
 from . import account_payment_order
 from . import account_payment_line
 from . import account_payment
