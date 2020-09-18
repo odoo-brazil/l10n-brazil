06b14bbc258ef9b8264138f773cdea2ce3fbf939
Gabriel Cardoso de Faria <gacarfaria@unifei.edu.br>
2020-09-18 13:54:23 -0300

    [REM] Old cnab option selection fields

--- l10n_br_account_payment_order/models/account_payment_order.py
+++ l10n_br_account_payment_order/models/account_payment_order.py
@@ -13,10 +13,7 @@ import logging
 from odoo import api, fields, models
 
 from ..constants import (
-    FORMA_LANCAMENTO,
-    FORMA_LANCAMENTO_ITAU,
     INDICATIVO_FORMA_PAGAMENTO,
-    TIPO_SERVICO,
 )
 
 _logger = logging.getLogger(__name__)
@@ -37,25 +34,6 @@ class AccountPaymentOrder(models.Model):
 
     cnab_filename = fields.Char(
         string='CNAB Filename',
-    )
-
-    service_type = fields.Selection(
-        selection=TIPO_SERVICO,
-        string='Tipo de Serviço',
-        help='Campo G025 do CNAB',
-        default='30',
-    )
-
-    release_form = fields.Selection(
-        selection=FORMA_LANCAMENTO,
-        string='Forma Lançamento',
-        help='Campo G029 do CNAB',
-    )
-
-    release_form_itau = fields.Selection(
-        selection=FORMA_LANCAMENTO_ITAU,
-        string='Forma Lançamento Itaú',
-        help='Campo G029 do CNAB',
     )
 
     code_convetion = fields.Char(
